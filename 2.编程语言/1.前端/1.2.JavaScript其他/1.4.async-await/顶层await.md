ce0f8f227433320ee93eb3376bb0808ad8992775
付祖敏 <fuzumin@foxmail.com>
2023-11-21 18:38:00 +0800

    示例

--- 2.编程语言/1.前端/1.2.JavaScript其他/1.4.async-await/顶层await.md
+++ 2.编程语言/1.前端/1.2.JavaScript其他/1.4.async-await/顶层await.md
@@ -8,3 +8,19 @@
   // 报错
   const data = await fetch('https://api.example.com');
   ```
+
++ 从 ES2022 开始，允许在模块的顶层独立使用 `await` 命令，使得上面那行代码不会报错了
++ 它的主要目的是使用await解决模块异步加载的问题
+
+  ```js
+  // a.js
+  const resp = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = resp.json();
+  export { users};
+
+  // usingAwait.js
+  import {users} from './a.mjs';
+
+  console.log(users);
+  console.log('usingAwait module');
+  ```
---
+++ 2.编程语言/9.1.Web3D/2.第一、三人称漫游.md/项目/model.js
@@ -0,0 +1,32 @@
+// @ts-check
+import * as THREE from "three";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const loader = new GLTFLoader();
+
+const group = new THREE.Group();
+
+/** @type {any} */
+let gltf = null;
+
+try {
+  gltf = await loader.loadAsync("./人.glb");
+  console.log('gltf', gltf);
+  const player = gltf.scene;//玩家角色模型
+  group.add(player)
+} catch (error) {
+  console.error(error);
+}
+console.log(12);
+// if(gltf === null) { return }
+console.log(1);
+//包含关键帧动画的模型作为参数创建一个播放器
+const mixer = new THREE.AnimationMixer(gltf.scene);
+console.log('gltf.animations', gltf.animations);
+// gltf.animations包含做个动作，选择其中的步行动作
+const clipAction = mixer.clipAction(gltf.animations[13]);
+clipAction.play(); //播放动画
+// console.log('gltf.scene', clipAction);
+
+console.log('mixer3', mixer);
+export { group, mixer };
