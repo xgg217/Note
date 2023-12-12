0dc055db20641d256b34033a0092ea819ad61e64
付祖敏 <fuzumin@foxmail.com>
2023-12-12 09:54:23 +0800

    添加新功能

--- 2.编程语言/9.1.Web3D/2.第一、三人称漫游.md/项目/index.js
+++ 2.编程语言/9.1.Web3D/2.第一、三人称漫游.md/项目/index.js
@@ -7,15 +7,28 @@ console.log(THREE.Scene)
 const scene = new THREE.Scene();
 
 // 2.创建相机
-const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
+const {camera, cameraGroup} = (() => {
+  const cameraGroup = new THREE.Group();
+  const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-// 设置相机位置
-// camera.position.set(5, 5, 5);
-// camera.lookAt(0, 0,0);
-// scene.add(camera)
-camera.position.set(0, 1.6, -5.5); // 玩家角色后面一点
-camera.lookAt(0, 1.6, 0);//对着人身上某个点  视线大致沿着人的正前方
-player.add(camera); // 第三人称视角：相机作为人的子对象，会跟着人运动
+
+  // 设置相机位置
+  // camera.position.set(5, 5, 5);
+  // camera.lookAt(0, 0,0);
+  // scene.add(camera)
+  camera.position.set(0, 1.6, -5.5); // 玩家角色后面一点
+  camera.lookAt(0, 1.6, 0);//对着人身上某个点  视线大致沿着人的正前方
+
+
+  return {
+    camera,
+    cameraGroup
+  }
+})();
+ // 层级关系：player <—— cameraGroup <—— camera
+ cameraGroup.add(camera);
+ player.add(cameraGroup); // 第三人称视角：相机作为人的子对象，会跟着人运动
+
 
 // 将几何体添加到场景中
 scene.add( group );
@@ -58,6 +71,9 @@ scene.add( gridHelper );
   const vMax = 5;//限制玩家角色最大速度
   const damping = -0.04; // 阻尼
   let leftButtonBool = false;//记录鼠标左键状态
+  // 上下俯仰角度范围
+  const angleMin = THREE.MathUtils.degToRad(-15);//角度转弧度
+  const angleMax = THREE.MathUtils.degToRad(15);
   const clock = new THREE.Clock();
 
   // 声明一个对象keyStates用来记录键盘事件状态
@@ -73,54 +89,48 @@ scene.add( gridHelper );
   // 当某个键盘按下设置对应属性设置为true
   document.addEventListener('keydown', (event) => {
     // 按下W键
-    if (event.code === 'KeyW') {
+    if (event.code === 'KeyW'  || event.code === 'ArrowUp') {
       keyStates.W = true;
-      return
     }
 
     // 按下A键
-    if (event.code === 'KeyA') {
+    if (event.code === 'KeyA' || event.code === 'ArrowLeft') {
       keyStates.A = true;
-      return
     }
 
     // 按下S键
-    if (event.code === 'KeyS') {
+    if (event.code === 'KeyS'  || event.code === 'ArrowDown') {
       keyStates.S = true;
-      return
     }
 
     // 按下D键
-    if (event.code === 'KeyD') {
+    if (event.code === 'KeyD'  || event.code === 'ArrowRight') {
       keyStates.D = true
-      return
     };
   });
 
   // 当某个键盘抬起设置对应属性设置为false
   document.addEventListener('keyup', (event) => {
+    // console.log('event.code', event.code);
+
     // 按下W键
-    if (event.code === 'KeyW') {
+    if (event.code === 'KeyW' || event.code === 'ArrowUp') {
       keyStates.W = false;
-      return
     }
 
     // 按下A键
-    if (event.code === 'KeyA') {
+    if (event.code === 'KeyA' || event.code === 'ArrowLeft') {
       keyStates.A = false;
-      return
     }
 
     // 按下S键
-    if (event.code === 'KeyS') {
+    if (event.code === 'KeyS' || event.code === 'ArrowDown') {
       keyStates.S = false;
-      return
     }
 
     // 按下D键
-    if (event.code === 'KeyD') {
+    if (event.code === 'KeyD' || event.code === 'ArrowRight') {
       keyStates.D = false
-      return
     };
   });
 
@@ -141,8 +151,22 @@ scene.add( gridHelper );
 
     //鼠标左键按下时候，才旋转玩家角色
     if(leftButtonBool){
-
+      // 左右旋转
       player.rotation.y -= event.movementX / 600;
+
+      // 鼠标上下滑动，让相机视线上下转动
+      // 相机父对象cameraGroup绕着x轴旋转,camera跟着转动
+      let x = cameraGroup.rotation.x - event.movementY / 600;
+
+      // 上下俯仰角度范围
+      if (cameraGroup.rotation.x < angleMin) {
+        x = angleMin;
+      }
+      if (cameraGroup.rotation.x > angleMax) {
+        x = angleMax
+      };
+
+      cameraGroup.rotation.x = x;
     }
   });
 
@@ -150,30 +174,33 @@ scene.add( gridHelper );
   // 循环执行的函数中测试W键盘状态值
   function render() {
     const deltaTime = clock.getDelta();
+    const front = new THREE.Vector3();
     if (v.length() < vMax) {//限制最高速度
 
       if(keyStates.W){
         console.log('W键按下');
         //先假设W键对应运动方向为z
-        const front = new THREE.Vector3();
+
         player.getWorldDirection(front);//获取玩家角色(相机)正前方
 
         // W键按下时候，速度随着时间增加
         // front.multiplyScalar(a * deltaTime) 速度的改变量
         // v（当前速度） + 速度的改变量
         v.add(front.multiplyScalar(a * deltaTime));
-
-
-        // return;
       }
 
       if(keyStates.S){
         // 与W按键相反方向
-        const front = new THREE.Vector3();
         player.getWorldDirection(front);
 
         // - a：与W按键反向相反
         v.add(front.multiplyScalar(-a * deltaTime));
+      }
+
+      if (keyStates.A) {//向左运动
+      }
+
+      if (keyStates.D) {//向右运动
       }
     }
 
