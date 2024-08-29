0f4ce895563eb183f9b68b6d69f2e1b69bc5019e
付祖敏 <1326580471@qq.com>
2024-08-29 16:21:20 +0800

    目录调整

---
+++ 1.前端编程/7.web图形技术/3D/webGL/7.图形绘制/图形/4.矩形/示例/utils.js
@@ -0,0 +1,41 @@
+export function initShaders(gl,vsSource,fsSource){
+  //创建程序对象
+  const program = gl.createProgram();
+  //建立着色对象
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+  //把顶点着色对象装进程序对象中
+  gl.attachShader(program, vertexShader);
+  //把片元着色对象装进程序对象中
+  gl.attachShader(program, fragmentShader);
+  //连接webgl上下文对象和程序对象
+  gl.linkProgram(program);
+  //启动程序对象
+  gl.useProgram(program);
+  //将程序对象挂到上下文对象上
+  gl.program = program;
+  return true;
+}
+
+function loadShader(gl, type, source) {
+  //根据着色类型，建立着色器对象
+  const shader = gl.createShader(type);
+  //将着色器源文件传入着色器对象中
+  gl.shaderSource(shader, source);
+  //编译着色器对象
+  gl.compileShader(shader);
+  //返回着色器对象
+  return shader;
+}
+
+/**
+ * 获取随机整数（不含最大值，含最小值）
+ * @param {number} min 最小值
+ * @param {number} max 最大值
+ * @returns {number}
+ */
+export function getRandomInt(min, max) {
+  const minVal = Math.ceil(min);
+  const maxVal = Math.floor(max);
+  return Math.floor(Math.random() * (maxVal - minVal)) + minVal; //不含最大值，含最小值
+}
