0e33540a309aa2eb83de222411f0f3aa28b4e291
付祖敏 <fuzumin@foxmail.com>
2024-09-20 23:16:51 +0800

    补充

--- 1.前端编程/7.web图形技术/3D/webGL/7.图形绘制/7.图形转面/示例/utils.js
+++ 1.前端编程/7.web图形技术/3D/webGL/7.图形绘制/7.图形转面/示例/utils.js
@@ -75,5 +75,18 @@ export const getMousePosInWebgl = (event, canvasElement) => {
   return {x, y}
 }
 
+//线性比例尺
+export function ScaleLinear(ax, ay, bx, by) {
+  const delta = {
+    x: bx - ax,
+    y: by - ay,
+  };
+  const k = delta.y / delta.x;
+  const b = ay - ax * k;
+  return function (x) {
+    return k * x + b;
+  };
+}
+
 
 
