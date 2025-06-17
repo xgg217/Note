5ad918d0e94cec4850790bbb70a4ad898d78951b
xgg217 <38310416+xgg217@users.noreply.github.com>
2025-06-18 00:24:12 +0800

    项目添加

---
+++ 1.前端编程/3.JavaScript/8.框架/Nuxt/测试/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import Test from "~/📁components"
+</script>
+
+<style lang="scss" scoped></style>
