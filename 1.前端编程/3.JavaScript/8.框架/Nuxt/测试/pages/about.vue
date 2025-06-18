858359b73ca4fb47722badf3215a9be281329b78
xgg217 <38310416+xgg217@users.noreply.github.com>
2025-06-18 23:24:27 +0800

    调整

--- 1.前端编程/3.JavaScript/8.框架/Nuxt/测试/pages/about.vue
+++ 1.前端编程/3.JavaScript/8.框架/Nuxt/测试/pages/about.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     12
+    <img src="~/assets/images/牛马.jpg" />
     <img :src="url" />
   </div>
 </template>
 
 <script setup lang="ts">
-const url = new URL("~/assets/images/nm.jpg", import.meta.url).href;
+const url = new URL("~/assets/images/牛马.jpg", import.meta.url).href;
 // const url = "~/assets/images/nm.jpg";
 </script>
 
